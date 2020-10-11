26e004bf22f0bff033e222065c4c152b04badfec
marcos <marcoslemeborbafilho@gmail.com>
2020-10-11 17:39:14 -0300

    second round

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 // This is an sample of JavaScript Code!!
 console.log('Hello World!');
+
+// This is related to 8.1 lesson, named "Variables"
+
+let name = 'Mosh';
+console.log(name);
+
+/* To naming variables in JS, 
+    1-They cannot be a reserved keyword. 
+    2-They should be related thru variable's propose.
+    3-They can not start with a number.
+    4-They can not contain space or hyphen.
+    5-They are case sensitive.
+    6-The best way to declare them is letting one line for each declared variable.
+*/
+
+const interestRate  = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+
+let name = 'Mosh';
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let lastName = null; // We use null in situations to clear the value of variable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
