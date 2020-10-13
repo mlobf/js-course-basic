77978b03a5091ce5ab98a73dda4e17e655e0eaca
marcos <marcoslemeborbafilho@gmail.com>
2020-10-12 23:29:35 -0300

    Last Commit on this Day.

--- operators.js
+++ operators.js
@@ -9,14 +9,18 @@ console.log(x % y);
 console.log(x ** y);
 
 //Increment (++)
+console.log(" Increment ++ ")
 console.log(x++);
 console.log(x);
 
 //Decrement
+
+console.log(" Decrement -- ")
 console.log(--x);
 
 //Increment with add.
 let z = 5;
+console.log(" Increment with add.")
 console.log(z += 5);
 console.log(z -= 5);
 console.log(z *= 5);
@@ -25,6 +29,7 @@ console.log(z **= 5);
 console.log(z %= 5);
 
 //Comparison Operator. 
+console.log(" Comparison Operator")
 let h = 1;
 
 //Relation Operators.
@@ -41,7 +46,51 @@ console.log(h !== 1);
 console.log(1 === 1);
 console.log('1' === 1);
 //Lose Equality. => (Type ! Value)
-console.log("Lose Equality")
+console.log(" Lose Equality")
 console.log(1 == 1);
 console.log('1' == 1);
 console.log(true == 1);
+
+//Ternary operators.
+// If a customer has more than 100 points,
+// they are 'gold' customers, otherwise,
+// they are 'silver' customer.
+
+let points = 90;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
+//Logical Operators.
+console.log(" Logical Operators")
+// Logical AND (&&)
+console.log('Logical AND')
+// Returns TRUE if both operands are TRUE
+console.log(true && true)
+
+//EligibleForLoan
+console.log('Eligible For Loan')
+/*
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome && goodCreditScore;
+
+console.log(eligibleForLoan);
+*/
+// Logical OR (||) 
+
+// This will return TRUE if one of operands id TRUE.
+
+console.log('Eligible For Loan')
+
+console.log('Logical OR')
+
+let highIncome = true;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome || goodCreditScore;
+
+console.log(eligibleForLoan);
+
+
+
+
+
