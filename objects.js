06ef6f2248937f040ec8d5a3496f3a0d42730915
marcos <marcoslemeborbafilho@gmail.com>
2020-10-17 20:45:22 -0300

    Objects Comes easy on OOP

---
+++ objects.js
@@ -0,0 +1,18 @@
+// Objects are collections if key value pairs.
+// Object-oriented Programming OOP.
+let radius = 1;
+let x = 1;
+let y = 1;
+
+const cicle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw');
+    }
+};
+cicle.draw(); //Method, because is related to an object.
