09970b5d19a6fe683ab6788ab3edc28485a3c592
marcos <marcoslemeborbafilho@gmail.com>
2020-10-21 19:38:48 -0300

    up to funk!!tioins

--- function.js
+++ function.js
@@ -1,1 +1,12 @@
 //Final Round
+console.log("-----------Lesson 91 ---- Functions Declarations VS Expressions-------------");
+
+//Function Declaration
+function walk(){
+    console.lop('walk');
+}
+
+//AnFunction Expression 
+let run = function(){
+    console.log('run');
+}
